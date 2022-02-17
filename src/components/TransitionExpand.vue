ba9bba2dc8b849a50f0a7cfa9ac400775686bac2
Konstantin Ladutenko <kostyfisik@gmail.com>
2022-02-17 16:10:39 +0300

    WAAPI initial

--- src/components/TransitionExpand.vue
+++ src/components/TransitionExpand.vue
@@ -1,5 +1,8 @@
 <template>
-  <Transition name="expand" @enter="enter" @leave="leave" @after-enter="afterEnter">
+  <Transition
+    name="expand" :css="false"
+    @enter="enter" @leave="leave"
+  >
     <slot />
   </Transition>
 </template>
@@ -15,37 +18,35 @@ export default defineComponent({
     const show = ref(false)
     return {
       show,
-      afterEnter(element: Element) {
-        console.log('after enter', element);
-
-        (element as HTMLElement).style.height = ''
-      },
-      enter(element: Element) {
-        const { height3 } = getComputedStyle(element)
+      enter(element: Element, done) {
         const { width } = getComputedStyle(element)
-
         element.style.width = width
         element.style.position = 'absolute'
         element.style.visibility = 'hidden'
         element.style.height = ''
 
         const { height } = getComputedStyle(element)
+
         element.style.width = ''
         element.style.position = ''
         element.style.visibility = ''
         element.style.height = '0px'
+        element.style.overflow = 'hidden'
 
-        // Force repaint to make sure the
-        // animation is triggered correctly.
-        // eslint-disable-next-line no-unused-expressions
-        getComputedStyle(element).height
+        const keyframes = [
+          { height: '0px', opacity: '0' },
+          { height, opacity: '1' },
+        ]
+        const options = { duration: 1250, easing: 'ease-in-out' }
 
-        requestAnimationFrame(() => {
-          // eslint-disable-next-line no-param-reassign
-          element.style.height = height
-        })
-        console.log('enter3', element)
+        const animation = element.animate(keyframes, options)
+        // We need to set a final height to 'auto' and we will do it right after
+        // launching the animation, as soon as it will not go live before the
+        // end of animation
+        element.style.height = ''
+        animation.onfinish = done()
       },
+
       leave(element: Element) {
         console.log('leave', element)
 
@@ -70,22 +71,5 @@ export default defineComponent({
 <style scoped>
 * {
   will-change: height;
-  transform: translateZ(0);
-  backface-visibility: hidden;
-  perspective: 1000px;
-}
-</style>
-
-<style>
-
-.expand-enter-active,
-.expand-leave-active {
-  transition: height 1s ease-in-out;
-  overflow: hidden;
-}
-
-.expand-enter,
-.expand-leave-to {
-  height: 0;
 }
 </style>
